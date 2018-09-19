9e91d71112bcbcc8027f4d1f2dd807e04d0e8c4a
jiazhuang <jia007119@163.com>
2018-09-19 11:31:34 +0800

    修改centos下的自动编译脚本

--- build_centos.sh
+++ build_centos.sh
@@ -1,34 +1,7 @@
-﻿#!/bin/sh
+#!/bin/sh
+
 #linux_installation_guide
-# befor compile, make sure you have installed dependent packages in your system
-# for Centos systems, do as following
-# default GCC 4.8.5 for Centos7.3.1611 or upgrade GCC to higher version
-# pre-installation:
-# install denpendancy packages
-#  >>>> sudo yum -y install cmake git readline-devel uuid-devel g++ ncurses-devel zip openssl openssl-devel pkgconfig build-essential python-dev autoconf autotools-devel libicu-devel libbz2-devel
-#  >>>> export LC_ALL="en_US.UTF-8"
-#-------------------------------------------------------------------------------------------             
-#    NOTE： Manually install Boost 1.59 and openssl 1.0.2k into default /usr/local directory             
-#     1).  install boost 1.59 
-#           wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz
-#           tar -zxvf boost_1_59_0.tar.gz
-#           cd boost_1_59_0
-#           ./bootstrap.sh  
-#           ./b2
-#           ./b2 install
-           
-#     2). install openssl 1.0.2k
-#           wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2k.tar.gz
-#           tar -zxvf openssl-1.0.2k.tar.gz
-#           cd openssl-1.0.2k
-#           ./config
-#           make
-#           make install
-#--------------------------------------------------------------------------------------------
-#
-echo "Warning: make sure you have installed dependent packages in your system"
-echo "if not, pls read the buildall.sh and run the comment scripts in the beginning"
-echo
+#Please run as root user!
 
 while true; do
     read -p "Do you wish to continue building the program?[y/n] " yn
@@ -39,41 +12,83 @@ while true; do
     esac
 done
 
-if [ "$1" != "download" ]
-then
-    echo "Do not download sourcecode.  Isdownload=Nodownload"
-    Isdownload=Nodownload
-else
-    Isdownload=$1
-fi
-
+#init the folder
 currentpath=$(pwd)
+miniupnpc_tar=$currentpath/miniupnpc-1.7.20120830.tar.gz
+boost_tar=$currentpath/boost_1_59_0.tar.gz
+leveldb_tar=$currentpath/v1.20.tar.gz
+
+miniupnpc_path=$currentpath/miniupnpc-1.7.20120830/
 leveldbpath=$currentpath/leveldb-1.20/
-miniupnpcpath=$currentpath/miniupnpc-1.7.20120830/
-leveldbtar=$currentpath/v1.20.tar.gz
-miniupnpctar=$currentpath/miniupnpc-1.7.20120830.tar.gz
 fc=$currentpath/fast-compile
-blockchain=$currentpath/Chain
+blockchain=$currentpath/SelfSell/Chain
 
-echo "build and  install the leveldb [1.18 or later]"
-if [ "$Isdownload" = "download" ]
-then
-    echo "download leveldb version 1.20 [https://github.com/google/leveldb/releases]"
-    wget -O v1.20.tar.gz https://github.com/google/leveldb/archive/v1.20.tar.gz
+#check file or folder exists
+
+if [ -f "$boost_tar" ]; then
+echo "$boost_tar exist"
 else
-    echo "Do not download leveldb source files"
+echo "ERROR--$boost_tar not exist"
+exit(0)
 fi
-echo
 
-if [ -f $leveldbtar ]; then 
+if [ -f "$leveldb_tar" ]; then
+echo "$leveldb_tar exist"
+else
+echo "ERROR--$leveldb_tar not exist"
+exit(0)
+fi
+
+if [ -f "$miniupnpc_tar" ]; then
+echo "$miniupnpc_tar exist"
+else
+echo "ERROR--$miniupnpc_tar not exist"
+exit(0)
+fi
+
+if [ -d "$fc" ]; then
+echo "$fc exist"
+else
+echo "ERROR--$fc not exist"
+exit(0)
+fi
+
+if [ -d "$blockchain" ]; then
+echo "$blockchain exist"
+else
+echo "ERROR--$blockchain not exist"
+exit(0)
+fi
+
+echo "Enter Y if you want to continue building the program when installing some dependent packages !!!"
+
+echo "[1/5] 1. start install boost，it will take 5-10 minutes..."
+
+export LC_ALL="en_US.UTF-8"
+tar -zxvf boost_1_59_0.tar.gz
+cd boost_1_59_0
+./bootstrap.sh  
+./b2
+
+mkdir /usr/local/boost_1_59_0
+mkdir /usr/local/boost_1_59_0/include
+mkdir /usr/local/boost_1_59_0/lib64
+cp -rf boost /usr/local/boost_1_59_0/include
+cp -rf stage/lib/* /usr/local/boost_1_59_0/lib64
+cd $currentpath/
+
+
+echo "[2/5] 2. start build and  install the leveldb [1.18 or later]..."
+
+if [ -f $leveldb_tar ]; then 
     echo "unzip leveldb source files"
     tar -zxvf v1.20.tar.gz
  else
     echo
 fi
 
-if [ -d "$leveldbpath" ]; then
-    cd $leveldbpath
+if [ -d "$leveldb_path" ]; then
+    cd $leveldb_path
     make
     sudo scp out-static/lib*  /usr/local/lib/
     cd ..
@@ -81,23 +96,16 @@ else
     echo "Error: there are no related leveldb files, pls check ..."
 fi
 
-echo "build and install the miniupnpc [ only  1.7 ]"
+echo "[3/5] 3. start  build and install the miniupnpc [ only  1.7 ]..."
 
-if  [ "$Isdownload" = "download" ] ; then
-    echo "download http://miniupnp.free.fr/files/download.php?file=miniupnpc-1.7.20120830.tar.gz"
-    wget -O miniupnpc-1.7.20120830.tar.gz http://miniupnp.free.fr/files/download.php?file=miniupnpc-1.7.20120830.tar.gz
-else
-    echo "Do not download miniupnpc files"
-fi
-
-if [ -f $miniupnpctar ]; then 
+if [ -f $miniupnpc_tar ]; then 
     echo "unzip miniupnpc..."
     tar -zxvf miniupnpc-1.7.20120830.tar.gz
 else
     echo
 fi
-if [ -d "$miniupnpcpath" ]; then
-    cd $miniupnpcpath
+if [ -d "$miniupnpc_path" ]; then
+    cd $miniupnpc_path
     cmake .
     make
     sudo make install
@@ -106,28 +114,21 @@ else
     echo "Error: there are no related miniupnpc files, pls check ..."
 fi 
 
-echo "build fast-compile library..."
-if  [ "$Isdownload" = "download" ] ; then
-    git clone https://github.com/SelfSell-Dev/fast-compile.git
-    cd fast-compile
-    git submodule update --init --recursive
-    git checkout static_variant_string_tag
-else
-    echo
-fi
+echo "[4/5] 4. start build fast-compile library，it will task a moment..."
+
 if [ -d "$fc" ]; then
     cd $fc
     cmake .
     make
-    sudo cp libfc.a  /usr/local/lib/
-    sudo cp $fc/vendor/secp256k1-zkp/src/project_secp256k1-build/.libs/libsecp256k1.a /usr/local/lib
+    cp libfc.a  /usr/local/lib/
+    cp $fc/vendor/secp256k1-zkp/src/project_secp256k1-build/.libs/libsecp256k1.a /usr/local/lib
     cd ..
 else
     echo "Error: no related fast-compile files, pls check..."
 fi
 
 echo
-echo "build SelfSell linux code..."
+echo "[5/5] 5. start build SelfSell,it will take 5-10 minutes ..."
 echo
 
 if [ -d "$blockchain" ]; then
@@ -139,5 +140,4 @@ else
     echo "Error: no related SelfSell_linux files, pls check..."
 fi
 
-echo "Info: finished building work, pls move $blockchain/SelfSell to the running directory "
-
+echo "Info: finished work successfully, please move $blockchain/SelfSell to the running directory "
